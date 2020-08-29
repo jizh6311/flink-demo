e79c0f88dec245ec24e7115d8f66e2fc7069f741
jizh6311 <zjwhut2010@hotmail.com>
2020-08-29 17:35:46 -0600

    Update start/stop scripts to select Flink version

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,1 +1,17 @@
-/usr/local/Cellar/apache-flink/1.9.1/libexec/bin/start-cluster.sh
+#!/bin/bash
+versionString=$(ls /usr/local/Cellar/apache-flink)
+versions=($(echo "$versionString" | tr '\s' '\n'))
+numberOfVersions="${#versions[@]}"
+if [[ $numberOfVersions -eq 1 ]]
+then
+  echo "Start Flink version ${versions[0]}"
+  /usr/local/Cellar/apache-flink/${versions[0]}/libexec/bin/start-cluster.sh
+else
+  echo -n "You have multiple Flink versions. Please select:"
+  select version in ${versions[@]}
+  do
+    echo "Start Flink version $version"
+    /usr/local/Cellar/apache-flink/${version}/libexec/bin/start-cluster.sh
+    break
+  done
+fi
--- stop-cluster.sh
+++ stop-cluster.sh
@@ -1,1 +1,17 @@
-/usr/local/Cellar/apache-flink/1.9.1/libexec/bin/stop-cluster.sh
+#!/bin/bash
+versionString=$(ls /usr/local/Cellar/apache-flink)
+versions=($(echo "$versionString" | tr '\s' '\n'))
+numberOfVersions="${#versions[@]}"
+if [[ $numberOfVersions -eq 1 ]]
+then
+  echo "Stop Flink version ${versions[0]}"
+  /usr/local/Cellar/apache-flink/${versions[0]}/libexec/bin/stop-cluster.sh
+else
+  echo -n "You have multiple Flink versions. Please select:"
+  select version in ${versions[@]}
+  do
+    echo "Stop Flink version $version"
+    /usr/local/Cellar/apache-flink/${version}/libexec/bin/stop-cluster.sh
+    break
+  done
+fi
